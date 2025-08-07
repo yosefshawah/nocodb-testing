81d0fb0c58f0470106cf345615d54cb0a3ec01fe
shawah Yosef <shawahyosef@shawahs-Mac-mini.local>
2025-08-07 22:17:12 +0300

    bugfix

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,13 +1,8 @@
 import requests
 
-BASE_URL = "http://localhost:8080"
+BASE_URL = "http://52.18.93.49:8080/"
 
-def test_server_up():
-    response = requests.get(BASE_URL)
-    assert response.status_code == 200 or response.status_code == 302  # 302 if redirecting to /dashboard
-
-def test_unauthorized_without_token():
-    url = f"{BASE_URL}/api/v2/tables"
-    response = requests.get(url)
-    assert response.status_code == 401
-    assert response.json().get("error") == "AUTHENTICATION_REQUIRED"
+class TestServerHealth:
+    def test_server_up(self):
+        response = requests.get(BASE_URL)
+        assert response.status_code in (200, 302)  # 302 if redirecting to /dashboard
